c8c6842979209a3edf20b4ce4ac5c661463e02fd
JotaDD <joaoeduardogomes@gmail.com>
2024-08-23 23:13:51 -0300

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       autoSchemaFile: true,
       sortSchema: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
