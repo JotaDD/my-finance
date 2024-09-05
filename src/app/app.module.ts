989d3910fd69a4c63278f8cba650ee70e1dc0976
JotaDD <joaoeduardogomes@gmail.com>
2024-09-05 17:34:27 -0300

    refactor: not using userModule at appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/infra/database/database.module';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
