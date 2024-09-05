d1fabef263162101257002d9fe12493c1e7550b6
JotaDD <joaoeduardogomes@gmail.com>
2024-09-05 17:34:47 -0300

    feat: add user entity

--- src/app/user/entities/user.entity.ts
+++ src/app/user/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User { }
+export class User {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  email: string;
+  hashedPassword: string;
+  hashedRefreshToken: string;
+  isActive: boolean;
+}
