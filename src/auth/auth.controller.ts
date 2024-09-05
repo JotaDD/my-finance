534bb28efa28802959b3a8589c346cee94875831
JotaDD <joaoeduardogomes@gmail.com>
2024-09-05 17:33:41 -0300

    feat: add signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { encode } from 'src/utils/bcrypt';
+import { SignUpDto } from './dto/signup.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  async signup(@Body() data: SignUpDto) {
+    const hashedPassword = await encode(data.password);
+    return await this.authService.signup({ ...data, password: hashedPassword });
+  }
+}
