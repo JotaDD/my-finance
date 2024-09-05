d124beff2471ec9e454b81f1cdd0dd70694a9db6
JotaDD <joaoeduardogomes@gmail.com>
2024-09-05 17:33:19 -0300

    refactor: delete unused lines

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/app/user/user.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService, UserService],
 })
 export class AuthModule {}
